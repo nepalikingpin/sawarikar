7e56e2a40e97d25af983bf7fdee77f5b6fe0176d
Manish Bhattarai <bhaimanish00@gmail.com>
2019-08-02 08:34:30 +0545

    Added Splash Screen and LoginRegister Activity

--- app/src/main/java/com/example/sawarikar/MainActivity.java
+++ app/src/main/java/com/example/sawarikar/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.sawarikar;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int SPLASH_TIME_OUT =700;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent loginIntent = new Intent(MainActivity.this, LoginRegisterActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
